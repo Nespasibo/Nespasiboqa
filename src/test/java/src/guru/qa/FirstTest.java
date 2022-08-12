c56c5515cfdaba96bc2598d00618ed6937fc5817
Nespasibo <Nikita.Brachun@playtech.com>
2022-08-12 17:26:42 +0300

    1st

--- src/test/java/src/guru/qa/FirstTest.java
+++ src/test/java/src/guru/qa/FirstTest.java
@@ -1,4 +1,15 @@
 package guru.qa;
+import org.junit.jupiter.api.Assertions;
+import org.testng.annotations.Test;
+
 
 public class FirstTest {
+    @Test
+    void firstTest() {
+        Assertions.assertTrue(condition: true);
+    }
+
+
+
+
 }
